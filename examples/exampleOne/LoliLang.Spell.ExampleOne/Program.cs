b671c888047baf4cc21a3ddff9c9fe90be2f7650
Daniel Freedman <deadpool1118@gmail.com>
2021-10-23 17:14:09 +0300

    add example

---
+++ examples/exampleOne/LoliLang.Spell.ExampleOne/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using LoliLang.Spell.Lexy;
+
+namespace LoliLang.Spell.ExampleOne
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var lexy = new Lexy.Lexy();
+
+            Console.WriteLine("Result: "+lexy.AnswersOn("if 2 / 2 == 1 then 1 else 0").Value);
+        }
+    }
+}
